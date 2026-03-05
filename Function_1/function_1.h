c43779692b2604b3e3f5303bbd230e98cd85eb58
flmpx <flmpx18773906537@163.com>
2026-03-05 22:41:48 +0800

    修改头文件的引入错误

--- Function_1/function_1.h
+++ Function_1/function_1.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_H
-#define FUNCTION_H
+#ifndef FUNCTION_1_H
+#define FUNCTION_1_H
 //传入一个起始值start,终点值end,其中会把区间矫正,改为0<=start<=end<allNum
 int letRangeToRight(int* start,int* end,int allNum);
 #endif
